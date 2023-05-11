9d7116413960fad5c5ef7647eb3693585d157c40
강상원 <kevink1113@u.sogang.ac.kr>
2023-05-12 02:17:17 +0900

    Admin page running

--- qr_app/admin.py
+++ qr_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from . import models
 # Register your models here.
+
+@admin.register(models.NewUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "username",
+                )
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "is_active",
+        "is_staff",
+    )
--- qr_app/models.py
+++ qr_app/models.py
@@ -4,39 +4,40 @@ from django.core.validators import RegexValidator
 
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
-
     username = models.CharField(max_length=150, unique=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
+
 class NewUserAccountManager(BaseUserManager):
 
-    def create_superuser(self,username, password, **other_fields):
+    def create_superuser(self, username, password, **other_fields):
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
-      
+
         if other_fields.get('is_staff') is not True:
             raise ValueError('Superuser must be assigned to is_staff=True')
-       
+
         if other_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must be assigned to is_superuser=True')
-        user =  self.create_user(username, password, **other_fields)
+        user = self.create_user(username, password, **other_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_user(self, username,password,**other_fields):
+    def create_user(self, username, password, **other_fields):
         if not email:
             raise ValueError('Email address is required!')
         email = self.normalize_email(email)
         if password is not None:
-            user = self.model(username=username,password=password, **other_fields)
+            user = self.model(username=username, password=password, **other_fields)
             user.save()
         else:
-            user = self.model(username=username, password=password,**other_fields)
+            user = self.model(username=username, password=password, **other_fields)
             user.set_unusable_password()
             user.save()
 

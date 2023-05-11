b4eca54331637082f73e62e710252aa7e5a7e0a9
otkmrecital <otkmrecital@gmail.com>
2023-05-11 17:48:24 +0900

    메이플 이야기: 메이플스토리는 지금 20주년 이벤트가 진행중이다

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from pybo.models import Student
+
+# Create your models here.
+
+
+class Post(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='post')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+

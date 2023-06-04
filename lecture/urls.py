a380d2f62b77ca43580d851012f9d6dd48f9754e
john9590 <john9590@naver.com>
2023-06-04 23:12:32 +0900

    lecture_edit complete & cron notice by new model

--- lecture/urls.py
+++ lecture/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', lecture_list, name='lecture_list'),
     path('select/<str:lecture_id>', lecture_select, name='lecture_detail'),
-    path('new/', lecture_new, name='lecture_new'),
-    path('edit/<str:lecture_id>', lecture_edit, name='lecture_edit'),
+    path('new/<str:name>/<str:advisor>/<str:classroom>/<str:day>/<str:start_time>/<str:end_time>', lecture_new, name='lecture_new'),
+    path('edit/<str:lecture_id>/<str:name>/<str:advisor>/<str:classroom>/<str:day>/<str:start_time>/<str:end_time>', lecture_edit, name='lecture_edit'),
     path('delete/<str:lecture_id>', lecture_delete, name='lecture_delete'),
 ]
--- lecture/views.py
+++ lecture/views.py
@@ -23,6 +23,7 @@ def lecture_list(request):
     lplace = []
     lecture = []
     lec_id = []
+    real = []
     for i in lectures.all():
         if i.course.semester == int(value):
             et = i.course.end_time
@@ -34,11 +35,13 @@ def lecture_list(request):
             adv.append(i.course.advisor)
             lname.append(i.course.name)
             lplace.append(i.course.classroom)
+            if i.real: real.append(1)
+            else: real.append(0)
     lec = Course.objects.filter(semester = '231')
     for i in lec.all():
         lecture.append(i.name)
         lec_id.append(i.course_id)
-    context = {'semester' : value, 'runtime' : runtime, 'time' : time, 'day' : day, 'adv' : json.dumps(adv), 'lname' : json.dumps(lname), 'lplace' : json.dumps(lplace), 'lecture': json.dumps(lecture), 'lec_id': json.dumps(lec_id)}    #context = {'lectures' : lec,'times': times, 'days': days}
+    context = {'semester' : value, 'runtime' : runtime, 'time' : time, 'day' : day, 'adv' : json.dumps(adv), 'lname' : json.dumps(lname), 'lplace' : json.dumps(lplace), 'lecture': json.dumps(lecture), 'lec_id': json.dumps(lec_id), 'real':real}    #context = {'lectures' : lec,'times': times, 'days': days}
     return render(request, 'timetable.html', context)
 
 
@@ -48,10 +51,11 @@ def lecture_select(request, lecture_id):
     takes=User.get_takes(request.user.student_id)
     for i in takes.all():
         if i.course.semester == lec.semester:
-            if i.course.start_time < lec.end_time and i.course.start_time >= lec.start_time:
-                return redirect('lecture_list')
-            if i.course.end_time <= lec.end_time and i.course.end_time > lec.start_time:
-                return redirect('lecture_list')
+            if i.course.day % 10 == lec.day % 10 or i.course.day / 10 == lec.day % 10 or i.course.day % 10 == lec.day / 10 or i.course.day / 10 == lec.day / 10:
+                if i.course.start_time < lec.end_time and i.course.start_time >= lec.start_time:
+                    return redirect('lecture_list')
+                if i.course.end_time <= lec.end_time and i.course.end_time > lec.start_time:
+                    return redirect('lecture_list')
     lecture = Takes()
     lecture.student = User.get_student_by_id(request.user.student_id)
     print(lec)
@@ -62,46 +66,74 @@ def lecture_select(request, lecture_id):
     lecture.save()
     return redirect('lecture_list')
 
+daytoint = {'월요일':1, '화요일':2, '수요일':3, '목요일':4, '금요일':5, '토요일':6, '일요일':7}
 @login_required
-def lecture_new(request):
+def lecture_new(request,name,advisor,classroom,day,start_time,end_time):
     print(request.user)
-    if request.method == 'POST':
-        #print(request.POST['name'])
-        form = Course()
-        form.course_id = None
-        form.semester = 231
-        form.name = request.POST['name']
-        form.day = request.POST['day']
-        form.start_time = request.POST['start_time']
-        form.end_time = request.POST['end_time']
-        form.classroom = request.POST['classroom']
-        form.advisor = request.POST['advisor']
-        form.major=None
-        form.save()
-        lecture = Takes()
-        lecture.student = User.get_student_by_id(request.user.student_id)
-        lecture.course=form
-        lecture.middle_grade=None
-        lecture.final_grade = None
-        lecture.real=False
-        lecture.save()
-        #else :
-        #else :
-        return redirect('lecture_list')
-    else:
-        form = Takes()
-    return render(request, 'lecture/create.html', {'form': form})
+    form = Course()
+    form.course_id = None
+    form.semester = 231
+    form.name = name
+    form.day = daytoint[day]
+    form.start_time = datetime.strptime(start_time,"%H:%M").time()
+    form.end_time = datetime.strptime(end_time,"%H:%M").time()
+    form.classroom = classroom
+    form.advisor = advisor
+    form.major=None
+    takes=User.get_takes(request.user.student_id)
+    for i in takes.all():
+        if i.course.semester == form.semester:
+            if i.course.day % 10 == form.day % 10 or (int)(i.course.day / 10) == form.day % 10 or i.course.day % 10 == form.day / 10 or (int)(i.course.day / 10) == form.day / 10:
+                if i.course.start_time < form.end_time and i.course.start_time >= form.start_time:
+                    return redirect('lecture_list')
+                if i.course.end_time <= form.end_time and i.course.end_time > form.start_time:
+                    return redirect('lecture_list')
+    form.save()
+    lecture = Takes()
+    lecture.student = User.get_student_by_id(request.user.student_id)
+    lecture.course=form
+    lecture.middle_grade=None
+    lecture.final_grade = None
+    lecture.real=False
+    lecture.save()
+    return redirect('lecture_list')
 
 
-def lecture_edit(request, lecture_id):
-    lecture = Course.get_course_by_id(lecture_id,231)
-    if request.method == 'POST':
-        form = Course(request.POST) 
-        lecture = form.save(commit=False)
-        lecture.save()
-        return redirect('lecture_detail', lecture_id=lecture.course_id)
-    else:
-        return render(request, 'lecture_edit.html', {'form': form})
+def lecture_edit(request, lecture_id,name,advisor,classroom,day,start_time,end_time):
+    take = User.get_takes(request.user.student_id)
+    form = Course()
+    form.course_id = None
+    form.semester = 231
+    form.name = name
+    form.day = daytoint[day]
+    form.start_time = datetime.strptime(start_time,"%H:%M").time()
+    form.end_time = datetime.strptime(end_time,"%H:%M").time()
+    form.classroom = classroom
+    form.advisor = advisor
+    form.major=None
+    takes=User.get_takes(request.user.student_id)
+    for i in takes.all():
+        if take.all()[int(lecture_id)].course.day == i.course.day and take.all()[int(lecture_id)].course.start_time == i.course.start_time and take.all()[int(lecture_id)].course.end_time == i.course.end_time : continue
+        print(i.course.name)
+        if i.course.semester == form.semester:
+            print(i.course.day)
+            if i.course.day % 10 == form.day % 10 or (int)(i.course.day / 10) == form.day % 10 or i.course.day % 10 == form.day / 10 or (int)(i.course.day / 10) == form.day / 10:
+                print(i.course.name)
+                if i.course.start_time < form.end_time and i.course.start_time >= form.start_time:
+                    return redirect('lecture_list')
+                if i.course.end_time <= form.end_time and i.course.end_time > form.start_time:
+                    return redirect('lecture_list')
+    form.save()
+    lecture = Takes()
+    take.all()[int(lecture_id)].delete()
+    lecture.student = User.get_student_by_id(request.user.student_id)
+    lecture.course=form
+    lecture.middle_grade=None
+    lecture.final_grade = None
+    lecture.real=False
+    lecture.save()
+    return redirect('lecture_list')
+
 
 
 def lecture_delete(request, lecture_id):
